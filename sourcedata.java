59f5dad8c6770d0cc6491c2a289c4f80f61e5929
Judith He <44380995+JudithHe@users.noreply.github.com>
2022-11-12 18:16:06 +0800

    Update sourcedata.java

--- sourcedata.java
+++ sourcedata.java
@@ -1,21 +1,33 @@
 package com.xueai8;
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 
 public class StreamingJob {
 
+	public static void main(String[] args) throws Exception {
 
-  public static void main(String[] args) throws Exception {
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-      StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		DataStream<Tuple4<Integer, String,Integer,String>> dataStream = env.readTextFile("/Users/judith/Project/StreamingData/data/nation.csv")
+				.flatMap(new Splitter());
 
-      DataStreamSource<String> dataStream = env.readTextFile("/Users/judith/Project/StreamingData/data/nation.csv");
+		dataStream.print();
 
-      dataStream.print();
+		env.execute("Window WordCount");
+	}
 
-      env.execute("Window WordCount");
-    }
+	public static class Splitter implements FlatMapFunction<String, Tuple4<Integer, String,Integer,String>> {
+		@Override
+		public void flatMap(String sentence, Collector<Tuple4<Integer, String,Integer,String>> out) throws Exception {
+			System.out.println(sentence);
+			String[] word = sentence.split("|");
+			out.collect(new Tuple4<Integer, String,Integer,String>(Integer.parseInt(word[0]), word[1],Integer.parseInt(word[2]),word[3]));
+
+		}
+	}
+
 }
