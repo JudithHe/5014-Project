d4334fba3bbda06f181b2b727b6dea040bfe0822
Judith He <44380995+JudithHe@users.noreply.github.com>
2022-11-12 18:35:00 +0800

    Update sourcedata.java

--- sourcedata.java
+++ sourcedata.java
@@ -12,15 +12,24 @@ public class StreamingJob {
 
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		DataStream<Tuple4<Integer, String,Integer,String>> dataStream = env.readTextFile("/Users/judith/Project/StreamingData/data/nation.csv")
-				.flatMap(new Splitter());
+		DataStream<String> ds = env.readTextFile("/Users/judith/Project/StreamingData/data/nation.csv");
 
-		dataStream.print();
+		// 通过map转换，将事件流中事件的数据类型变换为(word,1)元组形式
+		DataStream<Tuple4<String,String,String,String>> ds_map =
+				ds.map(new org.apache.flink.api.common.functions.MapFunction<String, Tuple4<String,String,String,String>>() {
+					@Override
+					public Tuple4<String,String,String,String> map(String s) throws Exception {
+						String[] word = s.split("\\|");
+						return new Tuple4<>(word[0],word[1],word[2],word[3]);
+					}
+				});
+		ds_map.print();
 
-		env.execute("Window WordCount");
+		// 执行
+		env.execute("flink keyBy transformatiion");
 	}
 
-	public static class Splitter implements FlatMapFunction<String, Tuple4<Integer, String,Integer,String>> {
+	/*public static class Splitter implements FlatMapFunction<String, Tuple4<Integer, String,Integer,String>> {
 		@Override
 		public void flatMap(String sentence, Collector<Tuple4<Integer, String,Integer,String>> out) throws Exception {
 			System.out.println(sentence);
@@ -28,6 +37,6 @@ public class StreamingJob {
 			out.collect(new Tuple4<Integer, String,Integer,String>(Integer.parseInt(word[0]), word[1],Integer.parseInt(word[2]),word[3]));
 
 		}
-	}
+	}*/
 
 }
