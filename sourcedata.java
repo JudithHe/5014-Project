59c76d63882ea116c323e17303db26bf557df5bb
Judith He <44380995+JudithHe@users.noreply.github.com>
2022-11-14 23:43:03 +0800

    Update sourcedata.java

--- sourcedata.java
+++ sourcedata.java
@@ -1,42 +1,105 @@
 package com.xueai8;
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.tuple.Tuple4;
+import apple.laf.JRSUIState;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.*;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.functions.co.CoProcessFunction;
 import org.apache.flink.util.Collector;
 
-public class StreamingJob {
+public class Nation {
 
 	public static void main(String[] args) throws Exception {
 
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		DataStream<String> ds = env.readTextFile("/Users/judith/Project/StreamingData/data/nation.csv");
+		DataStream<String> nation = env.readTextFile("/Users/judith/Project/StreamingData/data/nation.csv");
+		DataStream<String> customer = env.readTextFile("/Users/judith/Project/StreamingData/data/customer.csv");
 
-		// 通过map转换，将事件流中事件的数据类型变换为(word,1)元组形式
-		DataStream<Tuple4<String,String,String,String>> ds_map =
-				ds.map(new org.apache.flink.api.common.functions.MapFunction<String, Tuple4<String,String,String,String>>() {
+		// 通过map转换，将事件流中事件的数据类型变换为(0,ALGERIA,0, haggle. carefully final deposits detect slyly agai)元组形式
+		DataStream<Tuple2<Integer,String>> nation_map =
+				nation.map(new org.apache.flink.api.common.functions.MapFunction<String, Tuple2<Integer,String>>() {
 					@Override
-					public Tuple4<String,String,String,String> map(String s) throws Exception {
+					public Tuple2<Integer,String> map(String s) throws Exception {
 						String[] word = s.split("\\|");
-						return new Tuple4<>(word[0],word[1],word[2],word[3]);
+						return new Tuple2<>(Integer.valueOf(word[0]),word[1]);
 					}
 				});
-		ds_map.print();
+		DataStream<Tuple7<Integer,String,String, Integer,String,Double, String>> customer_map =
+				customer.map(new org.apache.flink.api.common.functions.MapFunction<String, Tuple7<Integer,String,String, Integer,String,Double,String>>() {
+					@Override
+					public Tuple7<Integer,String,String, Integer,String,Double,String> map(String s) throws Exception {
+						String[] word = s.split("\\|");
+						return new Tuple7<>(Integer.valueOf(word[0]),word[1],word[2],Integer.valueOf(word[3]),word[4],
+								Double.valueOf(word[5]),word[7]);
+					}
+				});
+		// keyBy转换，按key重分区
+		KeyedStream<Tuple2<Integer, String>, Integer> nation_keyed = nation_map.keyBy(value -> value.f0);
+		KeyedStream<Tuple7<Integer,String,String, Integer,String,Double,String>, Integer> customer_keyed = customer_map.keyBy(value -> value.f3);
+
+		//connect two keyedstream
+		nation_keyed.connect(customer_keyed)
+				.process(new JoinMatchResult())
+				.print();
+
+		// 输出
+		//nation_keyed.print();
 
 		// 执行
-		env.execute("flink keyBy transformatiion");
+		env.execute("Nation Job");
 	}
 
-	/*public static class Splitter implements FlatMapFunction<String, Tuple4<Integer, String,Integer,String>> {
-		@Override
-		public void flatMap(String sentence, Collector<Tuple4<Integer, String,Integer,String>> out) throws Exception {
-			System.out.println(sentence);
-			String[] word = sentence.split("|");
-			out.collect(new Tuple4<Integer, String,Integer,String>(Integer.parseInt(word[0]), word[1],Integer.parseInt(word[2]),word[3]));
+	//自定义实现CoProcessFunction
+	public static class JoinMatchResult extends CoProcessFunction<Tuple2<Integer,String>,
+			Tuple7<Integer,String,String, Integer,String,Double,String>,
+			String>{
+		private ValueState<Tuple2<Integer, String>> nationState;
+		private ValueState<Tuple7<Integer, String, String, Integer, String, Double, String>> customerState;
+		//有运行上下文的时候
 
+		@Override
+		public void open(Configuration parameters) throws Exception {
+			nationState  = getRuntimeContext().getState(
+					new ValueStateDescriptor<Tuple2<Integer, String>>("nation", Types.TUPLE(Types.INT, Types.STRING))
+			);
+			customerState = getRuntimeContext().getState(
+					new ValueStateDescriptor<Tuple7<Integer,String,String, Integer,String,Double,String>>("customer",
+							Types.TUPLE(Types.INT,Types.STRING, Types.STRING, Types.INT,Types.STRING,Types.DOUBLE,Types.STRING))
+			);
 		}
-	}*/
+
+		@Override
+		public void processElement1(Tuple2<Integer, String> value,  Context ctx, Collector<String> out) throws Exception {
+			//来的是nation tuple,进入这个实例的是具有相同nation key的tuple
+			if (customerState.value() != null){
+				out.collect("matched"+value+customerState.value());
+				//清空状态
+				//customerState.clear();
+			} else{
+				//更新状态
+				nationState.update(value);
+			}
+		}
+
+		@Override
+		public void processElement2(Tuple7<Integer, String, String, Integer, String, Double, String> value, Context ctx, Collector<String> out) throws Exception {
+			if (nationState.value() != null){
+				out.collect("process element2 matched"+nationState.value()+" "+value);
+				//清空状态
+				//nationState.clear();
+			} else{
+				//更新状态
+				customerState.update(value);
+			}
+		}
+
+		//处理nation里面的tuple（相同的key做相同的处理）
+
+	}
 
 }
